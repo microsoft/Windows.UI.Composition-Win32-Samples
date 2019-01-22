0c76b20f594ad86d728faf2741dfee124f7fb8f8
daneuber <daneuber@microsoft.com>
2019-01-22 15:03:16 -0800

    init commit for wpf/visuallayerintegration sample

---
+++ dotnet/WPF/VisualLayerIntegration/WinCompWPF/BarGraphUtility/Bar.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Composition;
+
+namespace BarGraphUtility
+{
+    class Bar
+    {
+        private Compositor _compositor;
+        private float _height;
+        private CompositionRectangleGeometry rectGeometry;
+        private CompositionRectangleGeometry rectOutlineGeometry;
+        private ShapeVisual shapeVisual;
+        private ShapeVisual shapeOutlineVisual;
+        private CompositionSpriteShape barVisual;
+
+        public CompositionBrush Brush { get; set; }
+        public float Height {
+            get { return _height; }
+            set {
+                _height = value;
+                if (Root != null)
+                {
+                    rectGeometry.Size = new Vector2(value, Width);
+                    rectOutlineGeometry.Size = new Vector2(value, Width);
+                }
+            }
+        }
+        public float Width { get; set; }
+        public float Value { get; set; }
+        public string Label { get; set; }
+
+        public ShapeVisual Root { get; private set; }
+        public ShapeVisual OutlineRoot { get; private set; }
+
+        public Bar(Compositor compositor, float maxBarHeight, float height, float width, string label, float value, CompositionBrush brush = null)
+        {
+            _compositor = compositor;
+
+            Height = height;
+            Width = width;
+            Value = value;
+            Label = label;
+
+            if (brush == null)
+            {
+                brush = compositor.CreateColorBrush(Colors.Blue);
+            }
+            Brush = brush;
+
+            CreateBar(maxBarHeight);
+        }
+
+        public void UpdateSize(float width, float height)
+        {
+            Width = width;
+            Height = height;
+
+            rectGeometry.Size = new Vector2(Height, Width);
+            rectOutlineGeometry.Size = new Vector2(Height, Width);
+        }
+
+        public void CreateBar(float maxHeight)
+        {
+            var strokeThickness = 8;
+
+            // Define shape visual for bar outline
+            shapeOutlineVisual = _compositor.CreateShapeVisual();
+            shapeOutlineVisual.Size = new System.Numerics.Vector2(maxHeight, maxHeight);
+            shapeOutlineVisual.RotationAngleInDegrees = -90f;
+
+            // Create geometry and shape for the bar outline
+            rectOutlineGeometry = _compositor.CreateRectangleGeometry();
+            rectOutlineGeometry.Size = new System.Numerics.Vector2(Height, Width); //reverse width and height since rect will be at a 90* angle
+            var barOutlineVisual = _compositor.CreateSpriteShape(rectOutlineGeometry);
+            barOutlineVisual.StrokeThickness = (float)strokeThickness;
+            barOutlineVisual.StrokeBrush = Brush;
+
+            shapeOutlineVisual.Shapes.Add(barOutlineVisual);
+
+            // Define shape visual 
+            shapeVisual = _compositor.CreateShapeVisual();
+            shapeVisual.Size = new System.Numerics.Vector2(maxHeight, maxHeight);
+            shapeVisual.RotationAngleInDegrees = -90f;
+
+            // Create rectangle geometry and shape for the bar
+            rectGeometry = _compositor.CreateRectangleGeometry();
+            rectGeometry.Size = new System.Numerics.Vector2(Height, Width); //reverse width and height since rect will be at a 90* angle
+            barVisual = _compositor.CreateSpriteShape(rectGeometry);
+            barVisual.FillBrush = Brush;
+            
+            shapeVisual.Shapes.Add(barVisual);
+
+            Root = shapeVisual;
+            OutlineRoot = shapeOutlineVisual;
+
+            // Add implict animation to bar
+            var implicitAnimations = _compositor.CreateImplicitAnimationCollection();
+            // Trigger animation when the size property changes. 
+            implicitAnimations["Size"] = CreateAnimation();
+            rectGeometry.ImplicitAnimations = implicitAnimations;
+            rectOutlineGeometry.ImplicitAnimations = implicitAnimations;
+        }
+
+        Vector2KeyFrameAnimation CreateAnimation()
+        {
+            Vector2KeyFrameAnimation animation = _compositor.CreateVector2KeyFrameAnimation();
+            animation.InsertExpressionKeyFrame(0f, "this.StartingValue");
+            animation.InsertExpressionKeyFrame(1f, "this.FinalValue");
+            animation.Target = "Size";
+            animation.Duration = TimeSpan.FromSeconds(1);
+            return animation;
+        }
+    }
+}
---
+++ dotnet/WPF/VisualLayerIntegration/WinCompWPF/HwndHostControl.xaml.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Windows.UI.Composition;
+using System.Windows;
+
+namespace WinCompWPF
+{
+    /// <summary>
+    /// Interaction logic for HwndHostControl.xaml
+    /// </summary>
+    public partial class HwndHostControl : HwndHost
+    {
+        public IntPtr hwndHost
+        {
+            get; private set;
+        }
+        object dispatcherQueue;
+        int hostHeight, hostWidth;
+        double hostDpiX, hostDpiY;
+        public Compositor Compositor { get; private set; }
+
+        private ICompositionTarget compositionTarget;
+        internal const int
+            WS_CHILD = 0x40000000,
+            WS_VISIBLE = 0x10000000,
+            LBS_NOTIFY = 0x00000001,
+            HOST_ID = 0x00000002,
+            LISTBOX_ID = 0x00000001,
+            WS_VSCROLL = 0x00200000,
+            WS_BORDER = 0x00800000;
+
+        public Visual Child
+        {
+            set
+            {
+                if (Compositor == null)
+                {
+                    InitComposition(hwndHost);
+                }
+                compositionTarget.Root = value;
+            }
+        }
+
+        public HwndHostControl(double height, double width, double dpiX, double dpiY)
+        {
+            hostHeight = (int)height;
+            hostWidth = (int)width;
+            hostDpiX = (double)dpiX;
+            hostDpiY = (double)dpiY;
+        }
+
+        /*
+         * Create window and content
+         */
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            // Create Window
+            hwndHost = IntPtr.Zero;
+            hwndHost = CreateWindowEx(0, "static", "",
+                                      WS_CHILD | WS_VISIBLE,
+                                      0, 0,
+                                      (int)(Width * hostDpiX / 96.0),
+                                      (int)(Height * hostDpiY / 96.0),
+                                      hwndParent.Handle,
+                                      (IntPtr)HOST_ID,
+                                      IntPtr.Zero,
+                                      0);
+
+            // Create Dispatcher Queue
+            dispatcherQueue = InitializeCoreDispatcher();
+
+            // Build Composition Tree of content
+            InitComposition(hwndHost);
+
+            return new HandleRef(this, hwndHost);
+        }
+
+        /*
+         * Create dispatcher queue
+         */
+        private object InitializeCoreDispatcher()
+        {
+            DispatcherQueueOptions options = new DispatcherQueueOptions();
+            options.apartmentType = DISPATCHERQUEUE_THREAD_APARTMENTTYPE.DQTAT_COM_STA;
+            options.threadType = DISPATCHERQUEUE_THREAD_TYPE.DQTYPE_THREAD_CURRENT;
+            options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+
+            object queue = null;
+            CreateDispatcherQueueController(options, out queue);
+            return queue;
+        }
+
+        /*
+         * Get compositor and target for hwnd
+         */
+        private void InitComposition(IntPtr hwndHost)
+        {
+            ICompositorDesktopInterop interop;
+
+            Compositor = new Compositor();
+            object iunknown = Compositor as object;
+            interop = (ICompositorDesktopInterop)iunknown;
+            IntPtr raw;
+            interop.CreateDesktopWindowTarget(hwndHost, true, out raw);
+
+            object rawObject = Marshal.GetObjectForIUnknown(raw);
+            compositionTarget = (ICompositionTarget)rawObject;
+
+            if (raw == null) { throw new Exception("QI Failed"); }
+
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            if(compositionTarget.Root != null)
+            {
+                compositionTarget.Root.Dispose();
+            }
+            DestroyWindow(hwnd.Handle);
+        }
+
+        protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_MOUSEMOVE = 0x0200;
+            const int WM_LBUTTONDOWN = 0x0201;
+            switch (msg)
+            {                
+                case WM_MOUSEMOVE:
+                    var pos = PointToScreen(new Point((short)(((int)lParam) & 0xffff), (short)(((int)lParam) >> 16)));
+                    RaiseHwndMouseMove(new HwndMouseEventArgs(pos));
+                    break;
+                case WM_LBUTTONDOWN:
+                    RaiseHwndMouseLClick(new HwndMouseEventArgs());
+                    break;
+            }
+
+            return base.WndProc(hwnd, msg, wParam, lParam, ref handled);
+        }
+
+        protected virtual void RaiseHwndMouseMove(HwndMouseEventArgs args)
+        {
+            var handler = MouseMoved;
+            if (handler != null)
+                handler(this, args);
+        }
+
+        protected virtual void RaiseHwndMouseLClick(HwndMouseEventArgs args)
+        {
+            var handler = MouseLClick;
+            if (handler != null)
+                handler(this, args);
+        }
+
+        public event EventHandler<HwndMouseEventArgs> MouseLClick;
+        public event EventHandler<HwndMouseEventArgs> MouseMoved;
+
+
+        #region PInvoke declarations
+
+        //typedef enum DISPATCHERQUEUE_THREAD_APARTMENTTYPE
+        //{
+        //    DQTAT_COM_NONE,
+        //    DQTAT_COM_ASTA,
+        //    DQTAT_COM_STA
+        //};
+        internal enum DISPATCHERQUEUE_THREAD_APARTMENTTYPE
+        {
+            DQTAT_COM_NONE = 0,
+            DQTAT_COM_ASTA = 1,
+            DQTAT_COM_STA = 2
+        };
+
+        //typedef enum DISPATCHERQUEUE_THREAD_TYPE
+        //{
+        //    DQTYPE_THREAD_DEDICATED,
+        //    DQTYPE_THREAD_CURRENT
+        //};
+        internal enum DISPATCHERQUEUE_THREAD_TYPE
+        {
+            DQTYPE_THREAD_DEDICATED = 1,
+            DQTYPE_THREAD_CURRENT = 2,
+        };
+
+        //struct DispatcherQueueOptions
+        //{
+        //    DWORD dwSize;
+        //    DISPATCHERQUEUE_THREAD_TYPE threadType;
+        //    DISPATCHERQUEUE_THREAD_APARTMENTTYPE apartmentType;
+        //};
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct DispatcherQueueOptions
+        {
+            public int dwSize;
+
+            [MarshalAs(UnmanagedType.I4)]
+            public DISPATCHERQUEUE_THREAD_TYPE threadType;
+
+            [MarshalAs(UnmanagedType.I4)]
+            public DISPATCHERQUEUE_THREAD_APARTMENTTYPE apartmentType;
+        };
+
+        //HRESULT CreateDispatcherQueueController(
+        //  DispatcherQueueOptions options,
+        //  ABI::Windows::System::IDispatcherQueueController** dispatcherQueueController
+        //);
+        [DllImport("coremessaging.dll", EntryPoint = "CreateDispatcherQueueController", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateDispatcherQueueController(DispatcherQueueOptions options,
+                                                [MarshalAs(UnmanagedType.IUnknown)]
+                                               out object dispatcherQueueController);
+
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(int dwExStyle,
+                                                      string lpszClassName,
+                                                      string lpszWindowName,
+                                                      int style,
+                                                      int x, int y,
+                                                      int width, int height,
+                                                      IntPtr hwndParent,
+                                                      IntPtr hMenu,
+                                                      IntPtr hInst,
+                                                      [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+        #endregion PInvoke declarations
+    }
+
+
+    public class HwndMouseEventArgs : EventArgs
+    {
+        public Point point { get; set; }
+
+        public HwndMouseEventArgs(Point point)
+        {
+            this.point = point;
+        }
+        public HwndMouseEventArgs(){        }
+    }
+
+
+#region COM Interop
+
+/*
+#undef INTERFACE
+#define INTERFACE ICompositorDesktopInterop
+    DECLARE_INTERFACE_IID_(ICompositorDesktopInterop, IUnknown, "29E691FA-4567-4DCA-B319-D0F207EB6807")
+    {
+        IFACEMETHOD(CreateDesktopWindowTarget)(
+            _In_ HWND hwndTarget,
+            _In_ BOOL isTopmost,
+            _COM_Outptr_ IDesktopWindowTarget * *result
+            ) PURE;
+    };
+*/
+[ComImport]
+    [Guid("29E691FA-4567-4DCA-B319-D0F207EB6807")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICompositorDesktopInterop
+    {
+        void CreateDesktopWindowTarget(IntPtr hwndTarget, bool isTopmost, out IntPtr test);
+    }
+
+    //[contract(Windows.Foundation.UniversalApiContract, 2.0)]
+    //[exclusiveto(Windows.UI.Composition.CompositionTarget)]
+    //[uuid(A1BEA8BA - D726 - 4663 - 8129 - 6B5E7927FFA6)]
+    //interface ICompositionTarget : IInspectable
+    //{
+    //    [propget] HRESULT Root([out] [retval] Windows.UI.Composition.Visual** value);
+    //    [propput] HRESULT Root([in] Windows.UI.Composition.Visual* value);
+    //}
+
+    [ComImport]
+    [Guid("A1BEA8BA-D726-4663-8129-6B5E7927FFA6")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+    public interface ICompositionTarget
+    {
+        Windows.UI.Composition.Visual Root
+        {
+            get;
+            set;
+        }
+    }
+
+    #endregion COM Interop
+
+}
+
