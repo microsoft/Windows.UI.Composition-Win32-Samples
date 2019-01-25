bbb97c8ef8e71e060735386ca7ea1a58cc3213e1
Jim Walker (WINDOWS) <jimwalk@microsoft.com>
2019-01-25 11:36:47 -0800

    init commit for WinForms/HelloComposition sample

---
+++ dotnet/WinForms/HelloComposition/HelloComposition/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HelloComposition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.compositionHostControl1 = new HelloComposition.CompositionHostControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add composition element";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // compositionHostControl1
+            // 
+            this.compositionHostControl1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.compositionHostControl1.Location = new System.Drawing.Point(164, 13);
+            this.compositionHostControl1.Name = "compositionHostControl1";
+            this.compositionHostControl1.Size = new System.Drawing.Size(624, 425);
+            this.compositionHostControl1.TabIndex = 1;
+            this.compositionHostControl1.Text = "compositionHostControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.compositionHostControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private CompositionHostControl compositionHostControl1;
+    }
+}
+
---
+++ dotnet/WinForms/HelloComposition/HelloComposition/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloComposition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            float size = random.Next(50, 150);
+            float offsetX = random.Next(0, (int)(compositionHostControl1.Width - size));
+            float offsetY = random.Next(0, (int)(compositionHostControl1.Height - 68.5 - size));
+            compositionHostControl1.AddElement(size, offsetX, offsetY);
+        }
+    }
+}
