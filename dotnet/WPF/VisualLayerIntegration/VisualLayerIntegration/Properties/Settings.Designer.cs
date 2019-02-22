b62de5b7fee3f4c7ff6fbc9808fdba495009a79b
Jim Walker (WINDOWS) <jimwalk@microsoft.com>
2019-02-22 15:36:39 -0800

    Update the default namespace for the project

--- dotnet/WPF/VisualLayerIntegration/VisualLayerIntegration/Properties/Settings.Designer.cs
+++ dotnet/WPF/VisualLayerIntegration/VisualLayerIntegration/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinCompWPFWinForms.Properties {
+namespace VisualLayerIntegration.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
