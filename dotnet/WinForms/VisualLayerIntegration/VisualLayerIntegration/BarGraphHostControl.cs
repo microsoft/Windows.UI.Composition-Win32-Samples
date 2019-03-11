7a81a59dfe6da86fec6da1ca3309d8a3b4fe56c3
Jim Walker (WINDOWS) <jimwalk@microsoft.com>
2019-03-11 12:38:47 -0700

    Format comments

--- dotnet/WinForms/VisualLayerIntegration/VisualLayerIntegration/BarGraphHostControl.cs
+++ dotnet/WinForms/VisualLayerIntegration/VisualLayerIntegration/BarGraphHostControl.cs
@@ -40,37 +40,34 @@ namespace VisualLayerIntegration
             MouseMoved += BarGraphHostControl_MouseMoved;
         }
 
-        /*
-        * Handle mouse movement
-        */
+        // Handle mouse movement.
         private void BarGraphHostControl_MouseMoved(object sender, HwndMouseEventArgs e)
         {
-            //Adjust light position
+            // Adjust light position.
             if (currentGraph != null)
             {
                 Point adjustedTopLeft = GetControlPointInDIP();
 
-                //Get point relative to control
+                // Get point relative to control.
                 Point relativePoint = new Point(e.point.X - adjustedTopLeft.X, e.point.Y - adjustedTopLeft.Y);
 
-                //Update light position
+                // Update light position.
                 currentGraph.UpdateLight(relativePoint);
             }
         }
 
         private Point GetControlPointInDIP()
         {
-            //Get bounds of hwnd host control
-            Point controlTopLeft = PointToScreen(new Point(0, 0));  //top left of control relative to screen
-            //Convert screen coord to DIP
+            // Get bounds of hwnd host control.
+            // Top left of control relative to screen.
+            Point controlTopLeft = PointToScreen(new Point(0, 0));
+            // Convert screen coord to DIP.
             var adjustedX = (int)(controlTopLeft.X / (currentDpiX / 96.0));
             var adjustedY = (int)(controlTopLeft.Y / (currentDpiY / 96.0));
             return new Point(adjustedX, adjustedY);
         }
 
-        /*
-        * Handle Composition tree creation and updates
-        */
+        // Handle Composition tree creation and updates.
         public void UpdateGraph(Customer customer)
         {
             var graphTitle = customer.FirstName + " Investment History";
