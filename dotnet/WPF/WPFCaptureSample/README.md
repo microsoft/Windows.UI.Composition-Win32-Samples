c01ad10876aa45264f2392ff59bd7e26412074df
Kelly Renner <krenner@microsoft.com>
2019-03-15 13:31:03 -0700

    Adding sample for WPF capture

---
+++ dotnet/WPF/WPFCaptureSample/README.md
@@ -0,0 +1,18 @@
+#----
+This WPF sample demonstrates how to create an experience for capturing windows, displays
+or invoking the system level picker.
+
+---
+## Insiders
+
+
+
+This sample uses new APIs available in 19H1 insider builds.
+HWND and HMON APIs are in the Windows.Graphics.Capture.Interop.h header
+
+
+[Windows Insider Preview Downloads](https://www.microsoft.com/en-us/software-download/windowsinsiderpreviewSDK)
+
+##More Information
+
+[Windows.Graphics.Capture Namespace] (https://docs.microsoft.com/uwp/api/windows.graphics.capture)
---
+++ dotnet/WPF/WPFCaptureSample/WPFCaptureSample/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Robmikh.WindowsRuntimeHelpers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.System;
+
+namespace WPFCaptureSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            _controller = CoreMessagingHelper.CreateDispatcherQueueControllerForCurrentThread();
+        }
+
+        private DispatcherQueueController _controller;
+    }
+}
