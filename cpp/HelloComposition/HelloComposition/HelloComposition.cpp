b34ed22281c6c8c9c5c0cc23331fc2fe52bb37ed
Jim Walker (WINDOWS) <jimwalk@microsoft.com>
2019-04-29 15:03:27 -0700

    Set window size

--- cpp/HelloComposition/HelloComposition/HelloComposition.cpp
+++ cpp/HelloComposition/HelloComposition/HelloComposition.cpp
@@ -102,7 +102,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	hInst = hInstance; // Store instance handle in our global variable
 
 	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+		CW_USEDEFAULT, 0, 900, 672, nullptr, nullptr, hInstance, nullptr);
 
 	if (!hWnd)
 	{
