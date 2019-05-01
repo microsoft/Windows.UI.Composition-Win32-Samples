ebe63f645e411878a016e761865558e4df7eb593
Jim Walker (WINDOWS) <jimwalk@microsoft.com>
2019-05-01 10:52:17 -0700

    Basic app setup

---
+++ dotnet/WinForms/AcrylicEffect/AcrylicEffect/CompositionHost.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.UI.Composition;
+
+namespace AcrylicEffect
+{
+    public partial class CompositionHost : Control
+    {
+        IntPtr hwndHost;
+        object dispatcherQueue;
+        protected ContainerVisual containerVisual;
+        protected Compositor compositor;
+        private ICompositionTarget compositionTarget;
+
+        public Visual Child
+        {
+            set
+            {
+                if (compositor == null)
+                {
+                    InitComposition(hwndHost);
+                }
+                compositionTarget.Root = value;
+            }
+        }
+
+        public CompositionHost()
+        {
+            // Get the window handle.
+            hwndHost = Handle;
+
+            // Create dispatcher queue.
+            dispatcherQueue = InitializeCoreDispatcher();
+
+            // Build Composition tree of content.
+            InitComposition(hwndHost);
+        }
+
+        private object InitializeCoreDispatcher()
+        {
+            DispatcherQueueOptions options = new DispatcherQueueOptions();
+            options.apartmentType = DISPATCHERQUEUE_THREAD_APARTMENTTYPE.DQTAT_COM_STA;
+            options.threadType = DISPATCHERQUEUE_THREAD_TYPE.DQTYPE_THREAD_CURRENT;
+            options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+
+            object queue = null;
+            CreateDispatcherQueueController(options, out queue);
+            return queue;
+        }
+
+        private void InitComposition(IntPtr hwndHost)
+        {
+            ICompositorDesktopInterop interop;
+
+            compositor = new Compositor();
+            object iunknown = compositor as object;
+            interop = (ICompositorDesktopInterop)iunknown;
+            IntPtr raw;
+            interop.CreateDesktopWindowTarget(hwndHost, true, out raw);
+
+            object rawObject = Marshal.GetObjectForIUnknown(raw);
+            compositionTarget = (ICompositionTarget)rawObject;
+
+            if (raw == null) { throw new Exception("QI Failed"); }
+
+            containerVisual = compositor.CreateContainerVisual();
+            Child = containerVisual;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        #region PInvoke declarations
+
+        //typedef enum DISPATCHERQUEUE_THREAD_APARTMENTTYPE
+        //{
+        //    DQTAT_COM_NONE,
+        //    DQTAT_COM_ASTA,
+        //    DQTAT_COM_STA
+        //};
+        internal enum DISPATCHERQUEUE_THREAD_APARTMENTTYPE
+        {
+            DQTAT_COM_NONE = 0,
+            DQTAT_COM_ASTA = 1,
+            DQTAT_COM_STA = 2
+        };
+
+        //typedef enum DISPATCHERQUEUE_THREAD_TYPE
+        //{
+        //    DQTYPE_THREAD_DEDICATED,
+        //    DQTYPE_THREAD_CURRENT
+        //};
+        internal enum DISPATCHERQUEUE_THREAD_TYPE
+        {
+            DQTYPE_THREAD_DEDICATED = 1,
+            DQTYPE_THREAD_CURRENT = 2,
+        };
+
+        //struct DispatcherQueueOptions
+        //{
+        //    DWORD dwSize;
+        //    DISPATCHERQUEUE_THREAD_TYPE threadType;
+        //    DISPATCHERQUEUE_THREAD_APARTMENTTYPE apartmentType;
+        //};
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct DispatcherQueueOptions
+        {
+            public int dwSize;
+
+            [MarshalAs(UnmanagedType.I4)]
+            public DISPATCHERQUEUE_THREAD_TYPE threadType;
+
+            [MarshalAs(UnmanagedType.I4)]
+            public DISPATCHERQUEUE_THREAD_APARTMENTTYPE apartmentType;
+        };
+
+        //HRESULT CreateDispatcherQueueController(
+        //  DispatcherQueueOptions options,
+        //  ABI::Windows::System::IDispatcherQueueController** dispatcherQueueController
+        //);
+        [DllImport("coremessaging.dll", EntryPoint = "CreateDispatcherQueueController", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateDispatcherQueueController(DispatcherQueueOptions options,
+                                                 [MarshalAs(UnmanagedType.IUnknown)]
+                                        out object dispatcherQueueController);
+
+        #endregion PInvoke declarations
+    }
+
+    #region COM Interop
+
+    /*
+    #undef INTERFACE
+    #define INTERFACE ICompositorDesktopInterop
+        DECLARE_INTERFACE_IID_(ICompositorDesktopInterop, IUnknown, "29E691FA-4567-4DCA-B319-D0F207EB6807")
+        {
+            IFACEMETHOD(CreateDesktopWindowTarget)(
+                _In_ HWND hwndTarget,
+                _In_ BOOL isTopmost,
+                _COM_Outptr_ IDesktopWindowTarget * *result
+                ) PURE;
+        };
+    */
+    [ComImport]
+    [Guid("29E691FA-4567-4DCA-B319-D0F207EB6807")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICompositorDesktopInterop
+    {
+        void CreateDesktopWindowTarget(IntPtr hwndTarget, bool isTopmost, out IntPtr test);
+    }
+
+    //[contract(Windows.Foundation.UniversalApiContract, 2.0)]
+    //[exclusiveto(Windows.UI.Composition.CompositionTarget)]
+    //[uuid(A1BEA8BA - D726 - 4663 - 8129 - 6B5E7927FFA6)]
+    //interface ICompositionTarget : IInspectable
+    //{
+    //    [propget] HRESULT Root([out] [retval] Windows.UI.Composition.Visual** value);
+    //    [propput] HRESULT Root([in] Windows.UI.Composition.Visual* value);
+    //}
+
+    [ComImport]
+    [Guid("A1BEA8BA-D726-4663-8129-6B5E7927FFA6")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+    public interface ICompositionTarget
+    {
+        Windows.UI.Composition.Visual Root
+        {
+            get;
+            set;
+        }
+    }
+    #endregion COM Interop
+}
