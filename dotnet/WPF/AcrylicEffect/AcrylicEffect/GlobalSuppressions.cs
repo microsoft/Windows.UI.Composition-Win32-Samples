6d334aa43185b9d14c2924b0cd352f79c1b28fd7
Abi Bennet <abbennet@microsoft.com>
2019-05-05 00:45:46 -0700

    Added suppression file

---
+++ dotnet/WPF/AcrylicEffect/AcrylicEffect/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Pending>", Scope = "type", Target = "~T:WinComp.Interop.User32.WM")]
+
